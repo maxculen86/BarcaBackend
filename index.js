431986dde2f329063642b4d45264ffe25ce932f9
maxculen86 <42255662+maxculen86@users.noreply.github.com>
2018-11-10 11:55:48 -0300

    Final version updated

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ request({
     method:"GET", 
     uri: "http://www.mocky.io/v2/5808862710000087232b75ac",
     json: true,
-}).then(function(response){
-    var data = response;
+}).then(function(apiclients){
+    var data = apiclients;
     
     //Get clients by ID
     app.get('/api/clients/getbyid/:id',(req, res) => {
@@ -56,6 +56,9 @@ request({
         const clientquery = jsonQuery(['clients[name=?]',req.params.name], {
             data:data
         }).value;
+
+        //If query fails...
+        if (!clientquery) res.status(404).send('The client with the given name was not found');
         
         //Checking user roles
         if (clientquery.role != 'admin') {
@@ -64,23 +67,25 @@ request({
             process.exit(1);
         }
         
-        //If query fails...
-        if (!clientquery) res.status(404).send('The client with the given name was not found');
-        
         //If OK, then...
-        console.log('### Policies linked to a given name ###');
+        console.log('### Client requested data... ###')
         console.log(clientquery);
+        console.log('### Policies linked to a given name ###');
+        console.log('Getting Policies...');
 
         //Then request policies API
         request({
             method:"GET", 
             uri: "http://www.mocky.io/v2/580891a4100000e8242b75c5",
             json: true,
-        }).then(function(policies){
+        }).then(function(apipolicies){
+            var data = apipolicies;
             //Query policies by the id from the current
+            console.log('* Policies getted');
+            console.log('* ClientID for client requested:');
             console.log(clientquery.id);
             const poliquery = jsonQuery(['policies[clientId=?]',clientquery.id], {
-                policies:policies
+                data:data
             }).value;
 
             //If query fails...
@@ -88,8 +93,8 @@ request({
             
             //If OK, then...
             console.log('### Policies query ###');
-            //console.log(poliquery);
-            //res.send(poliquery);
+            console.log(poliquery);
+            res.send(poliquery);
         });
     });
     //Get user linked to a policy number
@@ -97,33 +102,44 @@ request({
         method:"GET", 
         uri: "http://www.mocky.io/v2/580891a4100000e8242b75c5",
         json: true,
-    }).then(function(policies){
-        var pdata = policies;
-
+    }).then(function(apipolicies){
         app.get('/api/clients/getuserbypolicy/:id',(req, res) => {
             //Quering the clients response
+            var data = apipolicies;
+            console.log('### Getting user by policies ###');
+            console.log('* Policies getted!');
+            console.log('* PolicieID for client requested:');
             console.log(req.params.id);
             const pquery = jsonQuery(['policies[id=?]',req.params.id], {
-                pdata:pdata
-            }).value;
-
-            //If query fails...
-            if (!pquery) res.status(404).send('The policie with the given id was not found')
-            
-            const clientquery = jsonQuery(['clients[name=?]',pquery.clientId], {
                 data:data
             }).value;
 
-            //Checking user roles
+            //If query fails...
+            if (!pquery) res.status(404).send('The policie with the given id was not found');
+            
+            //If OK, then...
+            console.log('* Obtained Client ID:');
+            console.log(pquery.clientId);
+
+            //Quering the clients response
+            var data = apiclients;
+            const clientquery = jsonQuery(['clients[id=?]',pquery.clientId], {
+                data:data
+            }).value;
+
+            //If query fails...
+            if (!clientquery) res.status(404).send('No clients were found');
+
+            //If OK, check user roles..
             if (clientquery.role != 'admin') {
-                console.log('### The client has no permissions ###');
-                res.status(404).send('Client has no permissions for the data');
+                console.log('### Client has no permissions ###');
+                res.status(404).send('Client has no permissions to access');
                 process.exit(1);
             }
 
             //If OK, then...
             console.log('### User linked to a given name ###');
-            console.log(clientquery);
+            console.log(clientquery );
             res.send(clientquery);
         });
     });
